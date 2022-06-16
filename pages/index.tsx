ffc84513dd933d39a5d92fc1ef0c9309b60eeeb6
tandaraelena <tandaraelena@yahoo.com>
2022-06-16 17:54:12 +0100

    feat: integrate firebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,28 @@
+import { collection, getDocs } from 'firebase/firestore/lite';
+import { db } from '../utils/init-firebase';
+import { useEffect, useState } from 'react';
+import { Card } from '../components/homepage/card';
+
 const HomePage = () => {
-  // const allCategories = getAllCategories();
+  const [data, setData] = useState<any>();
+  useEffect(() => {
+    (async function getCategories() {
+      const categ = collection(db, 'check');
+      const dbSnapshot = await getDocs(categ);
+      const dbList = dbSnapshot.docs.map((doc) => doc.data());
+      setData(dbList);
+    })();
+  }, []);
+  console.log('data', data);
   return (
     <div>
       <h1>Home page</h1>
+      <div style={{ color: 'green' }}>
+        <Card />
+      </div>
       {/* <CategoriesList items={allCategories} /> */}
     </div>
   );
 };
+
 export default HomePage;

23f5faad83e8af079bc330d8de902b485fc9552b
tandaraelena <tandaraelena@yahoo.com>
2022-07-01 16:22:43 +0100

    chore: create context

---
+++ context/basket.tsx
@@ -0,0 +1,66 @@
+import React, { useContext, useReducer, useState } from 'react';
+import { shopReducer, ADD_PRODUCT, REMOVE_PRODUCT } from './reducer';
+
+export interface BasketContextProps {
+  products: Array<{ id: string; title: string; price: number; image: string }>;
+  cart: Array<{ id: string; title: string; price: number; image: string }>;
+  addProductToCart: (product: { id: string; title: string; price: number; image: string }) => void;
+  removeProductFromCart: (productId: string) => void;
+}
+
+const BasketContext = React.createContext<BasketContextProps>({
+  products: [
+    { id: 'p1', title: 'Gaming Mouse', price: 29.99, image: '/assets/desert.jpg' },
+    { id: 'p2', title: 'Harry Potter 3', price: 9.99, image: '/assets/drinks.jpg' },
+    { id: 'p3', title: 'Used plastic bottle', price: 0.99, image: '/assets/grill.jpg' },
+    { id: 'p4', title: 'Half-dried plant', price: 2.99, image: '/assets/soup.jpg' },
+  ],
+  cart: [],
+  addProductToCart: () => {},
+  removeProductFromCart: () => {},
+});
+
+interface BasketProviderProps {
+  children?: React.ReactNode;
+}
+
+const BasketProvider: React.FunctionComponent = ({ children }: BasketProviderProps) => {
+  const products = [
+    { id: 'p1', title: 'Gaming Mouse', price: 29.99, image: '/assets/desert.jpg' },
+    { id: 'p2', title: 'Harry Potter 3', price: 9.99, image: '/assets/drinks.jpg' },
+    { id: 'p3', title: 'Used plastic bottle', price: 0.99, image: '/assets/grill.jpg' },
+    { id: 'p4', title: 'Half-dried plant', price: 2.99, image: '/assets/soup.jpg' },
+  ];
+  const [cartState, dispatch] = useReducer(shopReducer, { cart: [] });
+
+  const addProductToCart = (product) => {
+    setTimeout(() => {
+      dispatch({ type: ADD_PRODUCT, product: product });
+    }, 100);
+  };
+
+  const removeProductFromCart = (productId) => {
+    setTimeout(() => {
+      dispatch({ type: REMOVE_PRODUCT, productId: productId });
+    }, 100);
+  };
+
+  return (
+    <BasketContext.Provider
+      value={{
+        products: products,
+        cart: cartState.cart,
+        addProductToCart: addProductToCart,
+        removeProductFromCart: removeProductFromCart,
+      }}
+    >
+      {children}
+    </BasketContext.Provider>
+  );
+};
+
+const useBasketContext: () => BasketContextProps = () => {
+  return useContext(BasketContext);
+};
+
+export { BasketProvider, useBasketContext };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import '../styles.css';
-
+import { BasketProvider } from '../context/basket';
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <BasketProvider>
+      <Component {...pageProps} />{' '}
+    </BasketProvider>
+  );
 }

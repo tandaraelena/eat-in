baed594e24ff7feb3978d786c5ac178ddbd6d74e
tandaraelena <tandaraelena@yahoo.com>
2022-07-22 11:13:36 +0100

    chore: add soups

--- pages/products/soups.tsx
+++ pages/products/soups.tsx
@@ -1,16 +1,27 @@
-import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { ProductCard } from '../../components/global/card';
 import { MenuNavBar } from '../../components/global/menu-navigation';
 import { NavBar } from '../../components/global/navigation';
+import { useBasketContext,Product } from '../../context/basket';
+import { products } from '../../context/utils';
 
 const Soups = () => {
-  const router = useRouter();
-  const activeLink = router?.pathname.split('/')[2];
-  console.log('activeLink', activeLink);
+  const context = useBasketContext();
+  const [product, setProduct] = useState<Product>();
+  const productAdded = product ? context.cart.find((itm) => itm.id === product.id) : undefined;
+  const cards = products.filter((itm) => itm.id.startsWith('soups'))
+
   return (
     <div>
       <NavBar />
       <MenuNavBar />
-      soups
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', marginTop: '30px' }}>
+        {cards.map((itm) => (
+          <div onClick={() => setProduct(itm)} key={itm.title}>
+            <ProductCard title={itm.title} image={itm.image} price={itm.price} kcal={itm.kcal} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

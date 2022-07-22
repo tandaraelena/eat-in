8338be373cdb5f7293dc5bc41cc317960bd8bab9
tandaraelena <tandaraelena@yahoo.com>
2022-07-22 16:09:30 +0100

    chore: refactor

--- pages/basket.tsx
+++ pages/basket.tsx
@@ -1,7 +1,11 @@
-import { useRouter } from 'next/router';
-import { FaRegArrowAltCircleLeft, FaPlusCircle, FaTrashAlt } from 'react-icons/fa';
-import { useBasketContext } from '../context/basket';
-import Image from 'next/image';
+import { useRouter } from "next/router";
+import {
+  FaRegArrowAltCircleLeft,
+  FaPlusCircle,
+  FaTrashAlt,
+} from "react-icons/fa";
+import { useBasketContext } from "../context/basket";
+import Image from "next/image";
 import {
   BasketHeader,
   BasketText,
@@ -14,21 +18,21 @@ import {
   ItemActions,
   ItemHr,
   BasketFooter,
-} from '../components/basket/styles';
+} from "../components/basket/styles";
 
 const Basket = () => {
   const router = useRouter();
   const context = useBasketContext();
-  const total = context.cart.reduce((prev,curr) => {
-    return prev + (curr.quantity * curr.price)
-  }, 0)
+  const total = context.cart.reduce((prev, curr) => {
+    return prev + curr.quantity * curr.price;
+  }, 0);
 
   return (
     <div>
       <BasketHeader>
         <FaRegArrowAltCircleLeft
           onClick={() => {
-            router.push('/');
+            router.push("/");
           }}
           size="30px"
           color="#9A9898"
@@ -37,32 +41,47 @@ const Basket = () => {
       </BasketHeader>
       <BasketContainer>
         <ItemsWrapper>
-          {context.cart.length === 0 ? 
-          <div>The basket is empty. Please add products.</div>
-          : context.cart.map((itm) => (
-            <BasketItemWrapper key={itm.id}>
-              <BasketItem>
-                <ItemInfo>
-                  <Image src={itm.image} width={100} height={100} />
-                  <div>
-                  <ItemTitle>{itm.title} ({itm.quantity})</ItemTitle>
-                  <div style={{marginLeft: '20px'}}>{itm.description}</div>
-                  </div>
-                </ItemInfo>
-                <div style={{marginLeft: '20px'}}>{itm.price}</div>
-              </BasketItem>
-              <ItemActions>
-                <FaPlusCircle size="20px" color="green" onClick={() => context.addProductToCart(itm)}/>
-                <FaTrashAlt size="20px" color="red" style={{ marginRight: '20px' }} onClick={() => context.removeProductFromCart(itm.id)}/>
-              </ItemActions>
-              <ItemHr />
-            </BasketItemWrapper>
-          ))}
+          {context.cart.length === 0 ? (
+            <div>The basket is empty. Please add products.</div>
+          ) : (
+            context.cart.map((itm) => (
+              <BasketItemWrapper key={itm.id}>
+                <BasketItem>
+                  <ItemInfo>
+                    <Image src={itm.image} width={100} height={100} />
+                    <div>
+                      <ItemTitle>
+                        {itm.title} ({itm.quantity})
+                      </ItemTitle>
+                      <div style={{ marginLeft: "20px" }}>
+                        {itm.description}
+                      </div>
+                    </div>
+                  </ItemInfo>
+                  <div style={{ marginLeft: "20px" }}>£{itm.price}</div>
+                </BasketItem>
+                <ItemActions>
+                  <FaPlusCircle
+                    size="20px"
+                    color="green"
+                    onClick={() => context.addProductToCart(itm)}
+                  />
+                  <FaTrashAlt
+                    size="20px"
+                    color="red"
+                    style={{ marginRight: "20px" }}
+                    onClick={() => context.removeProductFromCart(itm.id)}
+                  />
+                </ItemActions>
+                <ItemHr />
+              </BasketItemWrapper>
+            ))
+          )}
           {context.cart.length !== 0 && (
-          <BasketFooter>
-            <span>Total:</span>
-            <span>£{total}</span>
-          </BasketFooter>
+            <BasketFooter>
+              <span>Total:</span>
+              <span>£{total}</span>
+            </BasketFooter>
           )}
         </ItemsWrapper>
       </BasketContainer>

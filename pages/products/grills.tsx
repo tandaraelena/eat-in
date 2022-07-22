cd43acfbad84d43a1fd6abdc3a1c9248c65e07ad
tandaraelena <tandaraelena@yahoo.com>
2022-07-22 15:27:14 +0100

    chore: add grills

--- pages/products/grills.tsx
+++ pages/products/grills.tsx
@@ -1,16 +1,37 @@
-import { useRouter } from 'next/router';
-import { MenuNavBar } from '../../components/global/menu-navigation';
-import { NavBar } from '../../components/global/navigation';
+import { useState } from "react";
+import { ProductCard } from "../../components/global/card";
+import { Drawer } from "../../components/global/drawer";
+import { Footer } from "../../components/global/footer";
+import { MenuNavBar } from "../../components/global/menu-navigation";
+import { NavBar } from "../../components/global/navigation";
+import { MenuContainer } from "../../components/global/styles";
+import { Product } from "../../context/basket";
+import { products } from "../../context/utils";
 
 const Grills = () => {
-  const router = useRouter();
-  const activeLink = router?.pathname.split('/')[2];
-  console.log('activeLink', activeLink);
+  const [product, setProduct] = useState<Product>();
+  const cards = products.filter((itm) => itm.id.startsWith("grill"));
+
   return (
     <div>
       <NavBar />
       <MenuNavBar />
-      Grills
+      <MenuContainer>
+        {cards.map((itm) => (
+          <div onClick={() => setProduct(itm)} key={itm.title}>
+            <ProductCard
+              title={itm.title}
+              image={itm.image}
+              price={itm.price}
+              kcal={itm.kcal}
+            />
+          </div>
+        ))}
+      </MenuContainer>
+      {product && (
+        <Drawer product={product} cancel={() => setProduct(undefined)} />
+      )}
+      <Footer />
     </div>
   );
 };
